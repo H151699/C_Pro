13a8d70c467196f7430f4376fc63b67230c7b8b6
Andrew Day <151699@stud.hvl.no>
2019-03-13 00:42:05 +0100

    my first c# application

---
+++ ConsoleApp/ConsoleAppTest1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            */
+
+            // Add title of App
+            Console.Title = " 你好";
+
+            // Console.Beep(500, 1000);
+            Console.Write("What is your name?");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+
+            // set text position - middle of the screen.
+            Console.SetCursorPosition(45, 15);
+
+            Console.Clear();  // clear above text
+            Console.WriteLine("Hello " + name + "!");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to quit!");
+            Console.ReadKey();
+        }
+    }
+}
